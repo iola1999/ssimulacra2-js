afd3fda6456b3e444fe02e96ddb1a43163fcbe6f
iola1999 <iola1999@foxmail.com>
2022-12-11 01:21:49 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# ssimulacra2-js
+
+使用 js 实现 ssimulacra2 计算，实现方案基于 https://github.com/rust-av/ssimulacra2
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+interface Person {
+  name: string;
+  age: number;
+}
+
+const a: Person = {
+  name: "tom",
+  age: 23,
+};
+a;
+function f(p: any) {
+  p;
+}
+f;
+
+interface A {
+  key: string;
+}
+
+export default a;
